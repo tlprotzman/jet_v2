8a9dfe8d50744371867320b6e247ad7291df55f0
Tristan Protzman <tlprotzman@gmail.com>
2022-02-21 00:13:34 -0500

    centrality definitions

--- setup.h
+++ setup.h
@@ -7,6 +7,28 @@
 #include "TFile.h"
 #include "jetreader/reader/reader.h"
 
+/*
+ Centrality binning:
+ Bin       Centrality (16)   Centrality (9)
+16           80-100%
+ 0           75- 80%            70-80%
+ 1           70- 75%            60-70%
+ 2           65- 70%            50-60%
+ 3           60- 65%            40-50%
+ 4           55- 60%            30-40%
+ 5           50- 55%            20-30%
+ 6           45- 50%            10-20%
+ 7           40- 45%             5-10%
+ 8           35- 40%             0- 5%
+ 9           30- 35%
+10           25- 30%
+11           20- 25%
+12           15- 20%
+13           10- 15%
+14            5- 10%
+15            0-  5%
+*/
+
 // Tree variables
 typedef struct {
     double jet_eta, jet_phi;
