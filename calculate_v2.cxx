cb55c0983d2a7ec0a710012730559d74cac2ad12
Tristan Protzman <tlprotzman@gmail.com>
2022-05-25 16:32:39 -0400

    refactor v2 calculation

--- calculate_v2.cxx
+++ calculate_v2.cxx
@@ -1,238 +1,175 @@
-/*
-Tristan Protzman
-Lehigh University
-March 10, 2022
-tlprotzman@gmail.com
-*/
-
 #include "TROOT.h"
-#include "TCanvas.h"
 #include "TChain.h"
 #include "TF1.h"
-#include "TFile.h"
-#include "TH1D.h"
-#include "TH2D.h"
-#include "THStack.h"
-#include "TLatex.h"
-#include "TLegend.h"
+#include "TH1.h"
+#include "TH3.h"
+#include "TH3D.h"
 #include "TMath.h"
-#include "TGraphErrors.h"
-#include "TNamed.h"
+#include "TCanvas.h"
 #include "TStyle.h"
-#include "TTree.h"
+#include "TLatex.h"
 
-#include "setup.h"
-#include "histogram_data.h"
-#include "histogram_package.h"
 #include "jet_tree.h"
 #include "event_tree.h"
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+
+// Forward declarations
+TChain* load_files(std::string file_list);
+TH3* setup_histogram();
+void event_loop(TChain *chain, Event_Tree *event_tree, Jet_Tree *jet_tree, TH3 *relative_angle);
+void save_histogram(TH3 *hist, std::string outfile_name);
+void calculate_v2(TH1 *dphi, int cent_bin, int pt_bin);
+void bin_loop(TH3 *relative);
+void draw_plot(TH1 *dphi, int cent_bin, int pt_bin);
 
 int main(int argc, char **argv) {
-    // ROOT::EnableImplicitMT();
-    // TFile *file = TFile::Open("out.root");
-    TChain *chain = new TChain("jet_data"); // Set up chain to analyse
+    TChain *chain = load_files("in.list");
+
+    
+
+    // Set up trees for reading
+    Event_Tree *event_tree = new Event_Tree(chain, "event");    // Event wise quantities
+    event_tree->readable_tree();
+    Jet_Tree *hardcore_jet_tree = new Jet_Tree(chain, "hc");    // Hardcore jets
+    hardcore_jet_tree->readable_tree();
+    Jet_Tree *jet_tree = new Jet_Tree(chain, "all");            // All jets
+    jet_tree->readable_tree();
+    // return 0;
+
+    // Create momentum and centrality bins
+    TH3 *relative_angle = setup_histogram();
+
+    // Process events
+    event_loop(chain, event_tree, jet_tree, relative_angle);
+
+    bin_loop(relative_angle);
+
+    save_histogram(relative_angle, "relative_angles.root");
+    // delete relative_angle;
+    // delete event_tree;
+    // delete jet_tree;
+    // delete hardcore_jet_tree;
+    // delete chain;
+
+
+}
+
+TChain* load_files(std::string file_list) {
+    // Load trees to analyze
+    TChain *chain = new TChain("jet_data");
     std::ifstream files("in.list");
     std::string filePath;
-    while (std::getline(files, filePath)) {
+    while (files >> filePath) {
         chain->AddFile(filePath.c_str());
     }
     files.close();
+    return chain;
+}
 
-    // jet_tree_data datum;
-    // read_tree(chain, &datum);
-    Event_Tree *event_tree = new Event_Tree(chain, "event");
-    event_tree->readable_tree();
-    Jet_Tree *hardcore_jet_tree = new Jet_Tree(chain, "hc");
-    hardcore_jet_tree->readable_tree();
-    Jet_Tree *jet_tree = new Jet_Tree(chain, "all");
-    jet_tree->readable_tree();
-    // return -4;
+TH3* setup_histogram() {
+    int n_phi_bins = 50;
+    double phi_low = 0;
+    double phi_high = TMath::Pi();
 
-    size_t num_pt_bins = 8;
-    double pt_bin_upper_bound[8] = {5, 10, 15, 20, 25, 30, 35, 40};
-    std::vector<TH1D*> jet_relative_phi;
-    std::vector<TH1D*> jet_relative_phi_unfolded;
-    std::vector<double> calculated_v2;
-    double ep_resolution = 0;
-    long num_events = 0;
+    int n_pt_bins = 8;
+    double pt_low = 0;
+    double pt_high = 5 * n_pt_bins; // 5 GeV bins
 
-    bool use_hardcore = false;
+    int n_centrality_bins = 16;
+    double centrality_low = -0.5;
+    double centrality_high = 15.5;
 
-    for (size_t i = 0; i < num_pt_bins + 1; i++) {
-        jet_relative_phi.push_back(new TH1D(Form("jet_relative_phi_%i", i), "Jet Spectra", 25, 0, TMath::PiOver2()));
-        jet_relative_phi_unfolded.push_back(new TH1D(Form("jet_relative_phi_unfolded_%i", i), "Jet Spectra", 100, 0, TMath::TwoPi()));
-        calculated_v2.push_back(0);
-    }
-    
-    // Populate relative angles
+    TH3 *h = new TH3D("relative", "relative",
+                      n_phi_bins, phi_low, phi_high,
+                      n_pt_bins, pt_low, pt_high,
+                      n_centrality_bins, centrality_low, centrality_high);
+
+    h->GetXaxis()->SetTitle("Phi");
+    h->GetYaxis()->SetTitle("Momentum");
+    h->GetZaxis()->SetTitle("Centrality");
+
+    return h;
+}
+
+void event_loop(TChain *chain, Event_Tree *event_tree, Jet_Tree *jet_tree, TH3 *relative_angle) {
     for (Long64_t n = 0; n < chain->GetEntries(); n++) {
-        // if (n > 1000) {
-        //     break;
-        // }
-        if (n % 10/*0000*/ == 0) {
-            std::cout << "Processed " << n << " events" << std::endl;
+        if (n % 100000 == 0) {
+            printf("Processed %.1fM events\n", n / 1000000.);
         }
-        chain->GetEvent(n);
-        // std::cout << "centrality:\t" << event_tree->centrality << std::endl;
-        // if (event_tree->centrality < 6 || event_tree->centrality > 9) {  // accept between 50-30% central
-        //     continue;
-        // }
+        chain->GetEvent(n); // Get the next events
 
-        UInt_t selected_num_jets = jet_tree->num_jets;
-        if (selected_num_jets > 5) {
-            continue;
+        if (event_tree->centrality >= 16 || event_tree->centrality < 0) {
+            continue; // Either very peripheral or can't determine 
         }
-        double *selected_jet_phi = jet_tree->jet_phi;
-        double *selected_jet_subtracted_pt = jet_tree->jet_pt_median_subtracted;
-        // if (use_hardcore) {                                          // Todo - Reimplement hardcore jets
-        //     selected_num_jets = datum.num_hardcore_jets;
-        //     selected_jet_phi = datum.hardcore_jets_phi;
-        //     selected_jet_subtracted_pt = datum.hardcore_jets_subtracted_pt;
-        // }
-
-
-        for (int i = 0; i < selected_num_jets; i++) {
-            // continue;
-            // if (datum.all_jets_pt[i] < 10) {         // pT cut
-            //     continue;
-            // }
-            if (!use_hardcore && jet_tree->jet_charged_z[i] > 0.95) {       // Jet z cut
-                continue;
-            }
-
-
-            double jet_phi = jet_tree->jet_phi[i];
-            double relative = abs(jet_phi - event_tree->ep_east);   // TODO: should be better?
-            double other_relative = jet_phi - event_tree->ep_east;
-
-
-            if (other_relative < 0) {
-                other_relative += TMath::TwoPi();
-            }
-            int pt_bin = 0;
-
-            while (pt_bin < num_pt_bins && selected_jet_subtracted_pt[i] > pt_bin_upper_bound[pt_bin]) {pt_bin++;}   // Populate the appropriate momentum bin
-            jet_relative_phi_unfolded[pt_bin]->Fill(other_relative);
-
-
+        
+        // For each jet in the event...
+        for (int i = 0; i < jet_tree->num_jets; i++) {
+            double relative = jet_tree->jet_phi[i] - event_tree->ep_east;
             if (relative < 0) {
                 relative += TMath::TwoPi();
             }
-
-            while (relative > TMath::PiOver2()) {
-                relative -= TMath::PiOver2();
+            if (relative > TMath::Pi()) {
+                relative -= TMath::Pi();
             }
-
-            num_events++;
-            jet_relative_phi[pt_bin]->Fill(relative);
-            calculated_v2[pt_bin] += cos(2 * relative);
-            ep_resolution += cos(2 * (event_tree->ep_east - event_tree->ep_west));
+            relative_angle->Fill(relative, jet_tree->jet_pt_median_subtracted[i], event_tree->centrality);
         }
     }
-    
-    ep_resolution /= num_events;
-    ep_resolution = sqrt(ep_resolution) * sqrt(2);
-    std::cout << "ep_resolution: " << ep_resolution << std::endl;
-    for (size_t i = 0; i < num_pt_bins + 1; i++) {
-        calculated_v2[i] /= jet_relative_phi[i]->GetEntries();
-        calculated_v2[i] /= ep_resolution;
-        std::cout << "Bin " << i << " calculated V2: " << calculated_v2[i] << " from " << jet_relative_phi[i]->GetEntries() << " events" << std::endl;
-    }
+}
 
-    // Calculate V2
-    double *x = (double*)malloc(num_pt_bins + 1 * sizeof(double));
-    double *y  = (double*)malloc(num_pt_bins * sizeof(double));
-    double *ex = (double*)malloc(num_pt_bins * sizeof(double));
-    double *ey = (double*)malloc(num_pt_bins * sizeof(double));
-    std::vector<TF1*> v2_fits;
-    for (size_t i = 0; i < num_pt_bins + 1; i++) {
-        if (jet_relative_phi[i]->GetEntries() == 0) {
-            continue;
+void save_histogram(TH3 *hist, std::string outfile_name) {
+    TFile *outfile = new TFile(outfile_name.c_str(), "RECREATE");
+    hist->SetDirectory(outfile);
+    hist->Write();
+    outfile->Close();
+    delete outfile;
+}
+
+// Fits the data and extracts v2
+void calculate_v2(TH1 *dphi, int cent_bin, int pt_bin) {
+    if (dphi->GetEntries() == 0) {
+        return;
+    }
+    TF1 *v2_fit = new TF1(Form("v2_bin_%d_%d", cent_bin, pt_bin), "[0] * (1 + [1] * 2 * cos(2*x))", 0, TMath::Pi()); // Fit distribution on [0, pi]
+    v2_fit->SetParameters(1, 0);
+    v2_fit->SetParNames("offset", "v2");
+    dphi->Fit(Form("v2_bin_%d_%d", cent_bin, pt_bin)); // Run fitting
+    draw_plot(dphi, cent_bin, pt_bin);
+}
+
+// Loops over and determins v2 for each bin
+void bin_loop(TH3 *relative) {
+    for (int cent_bin = 1; cent_bin <= relative->GetZaxis()->GetNbins(); cent_bin++) {
+        relative->GetZaxis()->SetRange(cent_bin, cent_bin);
+        for (int pt_bin = 1; pt_bin <= relative->GetYaxis()->GetNbins(); pt_bin++) {
+            relative->GetYaxis()->SetRange(pt_bin, pt_bin);
+            TH1 *dphi = relative->Project3D("x");
+            calculate_v2(dphi, cent_bin, pt_bin);
         }
-        TF1 *v2_fit = new TF1(Form("v2_bin_%d", i), "[0] * (1 + [1] * 2 * cos(2*x))", 0, TMath::PiOver2());
-        v2_fit->SetParameters(1, 0);
-        v2_fit->SetParNames("offset", "v2");
-        jet_relative_phi[i]->Fit(Form("v2_bin_%d", i));
-        x[i] = i * 5 - 2.5;
-        ex[i] = 1;
-        y[i] = v2_fit->GetParameter("v2");
-        ey[i] = v2_fit->GetParError(v2_fit->GetParNumber("v2"));
-        v2_fits.push_back(v2_fit);
     }
+}
 
-    TCanvas *c = new TCanvas("", "", 1000, 1000);
-    c->SetMargin(0.15, 0.1, 0.1, 0.1);
-    TGraphErrors *v2_momentum_dependence = new TGraphErrors(num_pt_bins, x, y, ex, ey);
-    v2_momentum_dependence->GetXaxis()->SetTitle("P_{t} Bin");
-    v2_momentum_dependence->GetYaxis()->SetTitle("v_{2}");
-    v2_momentum_dependence->SetTitle("Momentum Dependence of v_{2}");
-    v2_momentum_dependence->SetMarkerSize(2.5);
-    v2_momentum_dependence->SetMarkerStyle(1);
-    v2_momentum_dependence->SetLineWidth(2);
-    v2_momentum_dependence->Draw("AP");
-    v2_momentum_dependence->GetYaxis()->SetRangeUser(-0.2, 0.3);
-    // v2_momentum_dependence->GetXaxis()->SetBinLabel()        // ??? bin number on a graph seems weird
-    c->SaveAs("plots/v2_momentum_dependence.png");
-    c->SaveAs("plots/v2_momentum_dependence.c");
-    std::cerr << "Reached here" << std::endl;
-    return 0;
-
-    
-
-
-
-    /*
-    histogram_package *v2_plots = new histogram_package();
-    histogram_data **
-
+void draw_plot(TH1 *dphi, int cent_bin, int pt_bin) {
     TCanvas c("", "", 1000, 1000);
-
-    jet_relative_phi->SetLineColor(kRed);
-
-    std::cout << jet_relative_phi->GetFunction("v2")->GetParameter("v2") << std::endl;
-
+    dphi->SetLineColor(kRed);
     gPad->SetMargin(0.14, 0.9, 0.9, 0.9);
-    jet_relative_phi->Draw("e");
-    jet_relative_phi->SetXTitle("#Delta#phi=|#phi^{jet}-#Psi_{2}|");
-    jet_relative_phi->SetYTitle("#frac{dN}{d#Delta#phi}");
-    v2_fit->SetLineColor(kRed);
-    jet_relative_phi->GetFunction("v2")->DrawF1(-1 * TMath::PiOver2(), TMath::PiOver2(), "C same");
-    v2_fit->SetParameters(jet_relative_phi->GetFunction("v2")->GetParameter("offset"), calculated_v2);
-    v2_fit->SetLineColor(kBlue);
-    v2_fit->DrawF1(-1 * TMath::PiOver2(), TMath::PiOver2(), "same");
+    dphi->Draw("e");
+    dphi->SetXTitle("#Delta#phi=|#phi^{jet}-#Psi_{2}|");
+    dphi->SetYTitle("#frac{dN}{d#Delta#phi}");
+    TF1 *func = dphi->GetFunction(Form("v2_bin_%d_%d", cent_bin, pt_bin));
+    func->DrawF1(0, TMath::Pi(), "C same");
     TLatex formula = TLatex();
     formula.SetTextSize(0.02);
-    // formula.DrawLatexNDC(0.35, 0.85, Form("Equation: Scale * (1 + 2v_{2} cos(2#phi))", jet_relative_phi->GetFunction("v2")->GetParameter("offset"), jet_relative_phi->GetFunction("v2")->GetParameter("v2")));
-    formula.DrawLatexNDC(0.35, 0.85, Form(" red: %.1f(1 + 2 * %.5f cos(2#Delta#phi))", jet_relative_phi->GetFunction("v2")->GetParameter("offset"), jet_relative_phi->GetFunction("v2")->GetParameter("v2")));
-    formula.DrawLatexNDC(0.35, 0.82, Form("blue: %.1f(1 + 2 * %.5f cos(2#Delta#phi))", jet_relative_phi->GetFunction("v2")->GetParameter("offset"), calculated_v2));
-    formula.DrawLatexNDC(0.35, 0.79, "30\%-50\% central");
+    // formula.DrawLatexNDC(0.35, 0.85, Form("Equation: Scale * (1 + 2v_{2} cos(2#phi))", dphi->GetFunction("v2")->GetParameter("offset"), dphi->GetFunction("v2")->GetParameter("v2")));
+    formula.DrawLatexNDC(0.35, 0.85, Form("%.1f(1 + 2 * %.5f cos(2#Delta#phi))", func->GetParameter("offset"), func->GetParameter("v2")));
+    // formula.DrawLatexNDC(0.35, 0.79, "30\%-50\% central");
+    // formula.DrawLatexNDC(0.35, 0.79, "30\%-50\% central");
     
-    formula.DrawLatexNDC(0.2, 0.2, "Red: Fitted to data");
-    formula.DrawLatexNDC(0.2, 0.15, "Blue: v_{2}= #LTcos(2#Delta#phi)#GT");
-    c.Print("plots/jet_relative_phi.png");
-    c.Print("plots/jet_relative_phi.c");
+    c.Print(Form("plots/slices/png/jet_relative_phi_cent_%d_pt_%d.png", cent_bin, pt_bin));
+    c.Print(Form("plots/slices/c/jet_relative_phi_cent_%d_pt_%d.c", cent_bin, pt_bin));
 
-    jet_relative_phi_unfolded->Draw("e");
-    v2_full_range->SetLineColor(kRed);
-    jet_relative_phi_unfolded->GetFunction("v2_full")->DrawF1(0, TMath::TwoPi(), "C same");
-    v2_full_range->SetParameters(jet_relative_phi_unfolded->GetFunction("v2_full")->GetParameter("offset"), calculated_v2);
-    v2_full_range->SetLineColor(kBlue);
-    v2_full_range->DrawF1(0, TMath::TwoPi(), "same");
-    // formula.DrawLatexNDC(0.35, 0.85, Form("Equation: Scale * (1 + 2v_{2} cos(2#phi))", jet_relative_phi_unfolded->GetFunction("v2")->GetParameter("offset"), jet_relative_phi_unfolded->GetFunction("v2")->GetParameter("v2")));
-    formula.DrawLatexNDC(0.35, 0.85, Form(" red: %.1f(1 + 2 * %.5f cos(2#Delta#phi))", jet_relative_phi_unfolded->GetFunction("v2_full")->GetParameter("offset"), jet_relative_phi->GetFunction("v2")->GetParameter("v2")));
-    formula.DrawLatexNDC(0.35, 0.82, Form("blue: %.1f(1 + 2 * %.5f cos(2#Delta#phi))", jet_relative_phi_unfolded->GetFunction("v2_full")->GetParameter("offset"), calculated_v2));
-    formula.DrawLatexNDC(0.35, 0.79, "30\%-50\% central");
-    
-    formula.DrawLatexNDC(0.2, 0.2, "Red: Fitted to data");
-    formula.DrawLatexNDC(0.2, 0.15, "Blue: v_{2}= #LTcos(2#Delta#phi)#GT");
-
-    c.Print("plots/jet_relative_phi_unfolded.png");
-    c.Print("plots/jet_relative_phi_unfolded.c");
-
-    return 0;
-    */
 }
